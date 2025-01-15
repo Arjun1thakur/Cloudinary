8c15ecdea8b3d0ecd154ff7f6b154c85da8b3edb
arjun1thakur <arjunthakur89096@gmail.com>
2025-01-15 13:25:19 +0530

    first commit

---
+++ app/cartoonify/page.jsx
@@ -0,0 +1,20 @@
+import CloudinaryWrapper from "../components/cloudinarywrapper";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Regular Image </h1>
+        <CloudinaryWrapper
+          cartoonify
+          width="900"
+          height="500"
+          src="https://res.cloudinary.com/do49htzms/image/upload/v1710360452/l9uovdmzefl94ahljmsm.jpg"
+          sizes="100vw"
+          alt="Cartoon"
+          className="rounded-lg"
+        />
+      </div>
+    </main>
+  );
+}
